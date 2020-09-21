f902b03275d5815b7e391991f605e3837a1a2a34
Toby Dragon <dr.toby.dragon@gmail.com>
2020-09-21 09:50:50 -0400

    first commit

---
+++ src/main/java/edu/ithaca/dragon/App.java
@@ -0,0 +1,13 @@
+package edu.ithaca.dragon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
